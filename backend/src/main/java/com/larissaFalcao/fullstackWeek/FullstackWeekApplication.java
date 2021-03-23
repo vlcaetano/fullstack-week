17e8087f5244f0a888723ba5e4f5779aa7432a60
vlcaetano <vitorlc95@gmail.com>
2021-03-23 14:41:24 -0300

    Project created

---
+++ backend/src/main/java/com/larissaFalcao/fullstackWeek/FullstackWeekApplication.java
@@ -0,0 +1,13 @@
+package com.larissaFalcao.fullstackWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackWeekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/larissaFalcao/fullstackWeek/FullstackWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissaFalcao.fullstackWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
