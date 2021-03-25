6ae81420cb282e38b0d0da86f9b929d739a77d8f
vlcaetano <vitorlc95@gmail.com>
2021-03-25 14:25:43 -0300

    Day 2 - adding CrossOrigin to backend

--- backend/src/main/java/com/larissaFalcao/fullstackWeek/controller/PessoaController.java
+++ backend/src/main/java/com/larissaFalcao/fullstackWeek/controller/PessoaController.java
@@ -3,6 +3,7 @@ package com.larissaFalcao.fullstackWeek.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.larissaFalcao.fullstackWeek.repository.PessoaRepository;
 
 @RestController
 @RequestMapping("/pessoas")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class PessoaController {
 
 	@Autowired
